6aca8fb35b91cd7b01b898090df3aec60bdc1a44
v08nike <teddan817@gmail.com>
2023-12-06 04:37:33 +0800

    chore: implemented multifile upload

--- ChunkFileUploader/Controllers/FileController.cs
+++ ChunkFileUploader/Controllers/FileController.cs
@@ -1,65 +1,157 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System;
+using System.IO;
 
 namespace ChunkFileUploader.Controllers
 {
     [ApiController]
     [Route("[controller]")]
-    public class FileController : ControllerBase
+    public class FileController(IConfiguration configuration, ILogger<FileController> logger) : ControllerBase
     {
+        private readonly string _uploadPath = configuration["UploadPath"];
+        private readonly ILogger<FileController> _logger = logger;
+
         [HttpPost]
         [Route("UploadFile")]
         public async Task<IActionResult> UploadFile()
         {
-            var totalChunks = int.Parse(Request.Form["totalChunks"]);
-            var currentChunk = int.Parse(Request.Form["currentChunk"]);
-            var totalBytes = int.Parse(Request.Form["totalBytes"]);
-            var fileName = Request.Form["fileName"].ToString();
 
-            if (totalChunks >= currentChunk && (fileName != "" || fileName != null)  && Request.Form.Files.Count > 0 && totalBytes > 0)
+            string strFileInfo = Request.Form["fileInfo"].ToString();
+
+            if (strFileInfo == null)
             {
-                var uploadPath = Path.Combine(Directory.GetCurrentDirectory(), "uploads");
-
-                if (!Directory.Exists(uploadPath))
-                {
-                    Directory.CreateDirectory(uploadPath);
-                }
-
-                var filePath = Path.Combine(uploadPath, fileName + ".temp");
-
-                if (System.IO.File.Exists(filePath) && currentChunk == 0)
-                {
-                    System.IO.File.Delete(filePath);
-                }
-                
-                using (var fileStream = new FileStream(filePath, currentChunk == 0 ? FileMode.Create : FileMode.Append))
-                {
-                    await Request.Form.Files[0].CopyToAsync(fileStream);
-                }
-
-                if (currentChunk == totalChunks)
-                {
-                    FileInfo fileInfo = new FileInfo(filePath);
-
-                    if (totalBytes == fileInfo.Length)
-                    {
-                        string newFileNameWithoutExtension = Path.GetFileNameWithoutExtension(fileName);
-                        string fileExtension = Path.GetExtension(fileName);
-                        string newFileName = $"{newFileNameWithoutExtension}_{((DateTimeOffset)DateTime.Now).ToUnixTimeSeconds()}{fileExtension}";
-                        string newFilePath = Path.Combine(uploadPath, newFileName);
-
-                        System.IO.File.Move(filePath, newFilePath);
-                        return Ok(new { status = "success", message = "File uploaded successfully!" });
-                    } else
-                    {
-                        return Ok(new { status = "failed", message = "Sorry, File is not uploaded correctly! Please try again." });
-                    }
-                }
-
-                return Ok(new { status = "chunk_file_uploaded", message = "Chunk uploaded successfully!", currentChunk, nextChunk = currentChunk + 1 });
+                LogError("Missing Parameters!");
+                return Ok(new { status = "fail", message = "Missing Parameters, Please try again with correct params!" });
             }
 
-            return Ok(new { status = "failed", message = "Sorry, uploaded file or chunk is invailed. Try again" });
+            if (!Directory.Exists(_uploadPath))
+            {
+                Directory.CreateDirectory(_uploadPath);
+            }
+
+            List<FileInfomation> fileInfos = JsonConvert.DeserializeObject<List<FileInfomation>>(strFileInfo);
+
+            List<ReturnInfo> returnInfos = new List<ReturnInfo>();
+
+            if (fileInfos != null && fileInfos.Count > 0)
+            {
+                int index = 0;
+
+                foreach (FileInfomation fileInfo in fileInfos)
+                {
+                    var tempFileName = fileInfo.tempFileName == "" ? fileInfo.name + DateTimeOffset.UtcNow.ToUnixTimeMilliseconds() + ".temp" : fileInfo.tempFileName;
+                    var filePath = Path.Combine(_uploadPath, tempFileName);
+
+                    using (var fileStream = new FileStream(filePath, fileInfo.currentChunk == 0 ? FileMode.Create : FileMode.Append))
+                    {
+                        await Request.Form.Files[index].CopyToAsync(fileStream);
+                    }
+
+
+                    if (fileInfo.currentChunk == fileInfo.totalChunks)
+                    {
+                        FileInfo tempFile= new FileInfo(filePath);
+
+                        if (fileInfo.size == tempFile.Length)
+                        {
+                            string newFileNameWithoutExtension = Path.GetFileNameWithoutExtension(fileInfo.name);
+                            string fileExtension = Path.GetExtension(fileInfo.name);
+                            string newFileName = $"{newFileNameWithoutExtension}_{((DateTimeOffset)DateTime.Now).ToUnixTimeSeconds()}{fileExtension}";
+                            string newFilePath = Path.Combine(_uploadPath, newFileName);
+
+                            System.IO.File.Move(filePath, newFilePath);
+                            returnInfos.Add(new ReturnInfo { 
+                                currentChunk = fileInfo.currentChunk, 
+                                totalChunks = fileInfo.totalChunks, 
+                                fileIndex = fileInfo.fileIndex, 
+                                message = "File uploaded successfully!", 
+                                status = "success",
+                                name = fileInfo.name,
+                                size = fileInfo.size,
+                                tempFileName = tempFileName
+                            });
+
+                            Log(fileInfo.name + " File uploaded successfully!");
+
+                            index++;
+                            continue;
+                        }
+                        else
+                        {
+                            returnInfos.Add(new ReturnInfo
+                            {
+                                currentChunk = fileInfo.currentChunk,
+                                totalChunks = fileInfo.totalChunks,
+                                fileIndex = fileInfo.fileIndex,
+                                message = "Oops! Something went wrong during the file upload. Please try again.",
+                                status = "failed",
+                                name = fileInfo.name,
+                                size = fileInfo.size,
+                                tempFileName = tempFileName
+                            });
+
+                            LogError(fileInfo.name + ": Oops! Something went wrong during the file upload!");
+                            index++;
+                            continue;
+                        }
+                    }
+
+                    returnInfos.Add(new ReturnInfo
+                    {
+                        currentChunk = fileInfo.currentChunk,
+                        totalChunks = fileInfo.totalChunks,
+                        fileIndex = fileInfo.fileIndex,
+                        message = "Chunk uploaded successfully!",
+                        status = "chunk_file_uploaded",
+                        name = fileInfo.name,
+                        size = fileInfo.size,
+                        tempFileName = tempFileName
+                    });
+
+                    Log(fileInfo.name + " Chunk uploaded successfully!");
+
+                    index++;
+                }
+            }
+
+            return Ok(new { res = JsonConvert.SerializeObject(returnInfos) });
+        }
+
+        private void LogError(string errorMessage)
+        {
+            _logger.LogError(errorMessage);
+        }
+
+        private void Log(string errorMessage)
+        {
+            _logger.LogInformation(errorMessage);
+        }
+
+
+        class FileInfomation
+        {
+            public required int currentChunk { get; set; }
+            public int totalChunks { get; set; }
+            public required int size { get; set; }
+            public required string name { get; set; }
+            public required string tempFileName { get; set; }
+            public int fileIndex { get; set; }
+        }
+
+        class ReturnInfo
+        {
+            public required int currentChunk { get; set; }
+            public int totalChunks { get; set; }
+            public required int size { get; set; }
+            public required string name { get; set; }
+            public required string tempFileName { get; set; }
+            public int fileIndex { get; set; }
+            public required string status { get; set; }
+            public required string message { get; set; }
+
         }
     }
 }
--- ChunkFileUploader/Program.cs
+++ ChunkFileUploader/Program.cs
@@ -1,3 +1,5 @@
+using Serilog;
+
 namespace ChunkFileUploader
 {
     public class Program
@@ -9,6 +11,11 @@ namespace ChunkFileUploader
             // Add services to the container.
 
             builder.Services.AddControllers();
+
+            Log.Logger = new LoggerConfiguration()
+                .WriteTo.Console()
+                .WriteTo.File("error.log", rollingInterval: RollingInterval.Day)
+                .CreateLogger();
 
             var app = builder.Build();
 
